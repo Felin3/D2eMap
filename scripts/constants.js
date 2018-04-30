4ac501a4ee585ee831ff11e345905f06acc021b1
Felin3 <37182823+Felin3@users.noreply.github.com>
2018-04-30 12:34:28 +0200

    GH test

--- scripts/constants.js
+++ scripts/constants.js
@@ -1035,6 +1035,7 @@ VILLAGERS_LIST = [
 ];
 
 MAP_TILES_LIST_COMPLETE = [
+	['GH_Test',11,14],
 	['1',8,6],
 	['2',6,6],
 	['3',6,4],
--- scripts/main.js
+++ scripts/main.js
@@ -18,58 +18,6 @@ function addTextareaWithLabel(labelText, link) {
 	return html;
 }
 
-function Create_ActButton()
-{
-	var DisplayAct = "II";
-	if (CurrentAct == "I")
-	{
-		DisplayAct = "I";
-	}
-
-	var html;
-	html = $('<div>').addClass('SelectAct');
-	html.append('<input type="image" src="images/misc/Act' + DisplayAct + '.png" class="ImgAct" onclick="SwitchAct();" />');
-	return html;
-}
-
-function Fill_ActButton()
-{
-	var ActImg = $('.ImgAct');
-	var ActImgSrc = ActImg.attr('src');
-	ActImgSrc = ActImgSrc.replace('II','I');
-	ActImgSrc = ActImgSrc.replace('ActI', 'Act' + CurrentAct);
-	ActImg.attr('src', ActImgSrc);
-}
-
-function SwitchAct()
-{
-	var DisplayedAct = CurrentAct;
-	var SwitcToAct = "I";
-	if (CurrentAct == "I")
-	{
-		SwitcToAct = "II";
-	}
-
-	var ActImg = $('.ImgAct');
-	var ActImgSrc = ActImg.attr('src');
-	ActImgSrc = ActImgSrc.replace('Act' + DisplayedAct, 'Act' + SwitcToAct)
-	ActImg.attr('src', ActImgSrc);
-
-	//new current Act
-	CurrentAct = SwitcToAct;
-
-	adjustMonsterList();
-	UpdateWindow_MapControls();
-}
-
-function updateAct(NewAct) {
-	CurrentAct = NewAct;
-	Fill_ActButton();
-	adjustMonsterList();
-}
-
-
-
 function updateCoordinate(element, value) {
 	updateOption(element, value, false);
 }
